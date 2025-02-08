dc20f71621da150fa449548ecf2d182ef6bafba3
Ramanan <rubaramanan1997@gmail.com>
2025-02-08 15:53:18 -0500

    Design Door mat

---
+++ python/3. Strings/design_door_mat.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+inp = input()
+
+l = inp.split()
+
+n = int(l[0])
+m = int(l[1])
+c = '.|.'
+for i in range(n):
+    if i < n//2:
+        print((c+c*(i*2)).center(m, '-'))
+    if i == n//2:
+        print('WELCOME'.center(m, '-'))
+    if i > n//2:
+        x = n - (i - n//2) * 2
+        print((c*(x)).center(m, '-'))
