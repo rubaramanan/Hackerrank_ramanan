e4665073ef7978eb0b4afe367390cb2bfd0f2472
Ramanan <rubaramanan1997@gmail.com>
2025-02-14 07:00:51 -0500

    intro

---
+++ python/4. Sets/intro.py
@@ -0,0 +1,12 @@
+def average(array):
+    # your code goes here
+    array = list(set(array))
+    # print(len(array))
+    average = sum(array) / len(array)
+    return average
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
