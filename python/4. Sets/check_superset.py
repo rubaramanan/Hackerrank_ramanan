c38dcb5c549357bbda6c388834b927af7a7851ea
Ramanan <rubaramanan1997@gmail.com>
2025-03-06 22:36:52 -0500

    check superset

---
+++ python/4. Sets/check_superset.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def check_sperset(a,b):
+    return (a<=b) and (a!=b)
+
+b = set(map(int, input().split()))
+n = int(input())
+l = []
+for i in range(n):
+    a = set(map(int, input().split()))
+    l.append(check_sperset(a,b))
+
+print(all(l))
